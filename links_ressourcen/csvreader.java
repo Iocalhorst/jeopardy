f118b890450af5f3f8a88eac47f84f14d92689cb
Iocalhorst <55767052+Iocalhorst@users.noreply.github.com>
2021-11-17 16:29:47 +0100

    Update csvreader.java

--- links_ressourcen/csvreader.java
+++ links_ressourcen/csvreader.java
@@ -7,5 +7,27 @@ while ((row = csvReader.readLine()) != null) {
     String[] data = row.split(",");
     // do something with the data
 }
-csvReader.close();
+List<List<String>> rows = Arrays.asList(
+    Arrays.asList("Jean", "author", "Java"),
+    Arrays.asList("David", "editor", "Python"),
+    Arrays.asList("Scott", "editor", "Node.js")
+);
+
+FileWriter csvWriter = new FileWriter("new.csv");
+csvWriter.append("Name");
+csvWriter.append(",");
+csvWriter.append("Role");
+csvWriter.append(",");
+csvWriter.append("Topic");
+csvWriter.append("\n");
+
+for (List<String> rowData : rows) {
+    csvWriter.append(String.join(",", rowData));
+    csvWriter.append("\n");
 }
+
+csvWriter.flush();
+    
+    csvReader.close();
+}
+
