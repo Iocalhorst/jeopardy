c01f98a465d2c9c93a299d35ec45783de29b668d
Iocalhorst <55767052+Iocalhorst@users.noreply.github.com>
2021-11-17 16:28:03 +0100

    Update csvreader.java

--- links_ressourcen/csvreader.java
+++ links_ressourcen/csvreader.java
@@ -1,6 +1,11 @@
+
+File csvFile = new File(pathToCsv);
+if (csvFile.isFile()) {
+    // create BufferedReader and read data from csv
 BufferedReader csvReader = new BufferedReader(new FileReader(pathToCsv));
 while ((row = csvReader.readLine()) != null) {
     String[] data = row.split(",");
     // do something with the data
 }
 csvReader.close();
+}
